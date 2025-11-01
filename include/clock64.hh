acaae14cf7567904d509461bf1cf63a9ec40f701
Ethan Graham <ethan.w.s.graham@gmail.com>
2025-11-01 09:05:17 +0100

    clock64.cu: add helper for reading maximum clock64 frequency
    Adds a function, getMaxClockFrequencyHz(), that returns device 0's
    maximum clock frequency in Hz. In particular, this is the SM clock
    which corresponds to the frequency of the clock that clock64()
    probes.
    
    As this pulls in a new dependency for nvml, our workflows configuration
    is updated, as well as the Makefile that must link against it.

--- include/clock64.hh
+++ include/clock64.hh
@@ -6,4 +6,7 @@
 /** Host-callable wrapper. */
 double measureClock64Latency(uint64_t iters);
 
+/** Returns the maximum clock frequency in Hz. */
+unsigned int getMaxClockFrequencyHz();
+
 #endif /* CLOCK64_HH */
