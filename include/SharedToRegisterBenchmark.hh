113aa115c44c28dec4626bee0395eac0c0100381
PopoDev <popocompte@gmail.com>
2025-12-04 11:18:22 +0100

    shared_to_register: save multiple run for error bars

--- include/SharedToRegisterBenchmark.hh
+++ include/SharedToRegisterBenchmark.hh
@@ -19,9 +19,9 @@ class SharedToRegisterBenchmark {
   SharedToRegisterBenchmark(Encoder& e, const std::vector<std::string>& args = {}) : enc_(e) {
     benchmark::ArgParser parser(args);
     sizes_ = parser.getOr("sizes", std::vector<uint64_t>{4096, 8192, 16384, 32768 });
-    threads_ = parser.getOr("threads", std::vector<uint64_t>{32, 64, 128, 256, 512});
+    threads_ = parser.getOr("threads", std::vector<uint64_t>{32, 64, 128, 256, 512 });
     strides_ = parser.getOr("strides", std::vector<uint64_t>{1, 2, 4, 8, 16, 32});
-    elemBytes_ = 4UL;
+    elemBytes_ = 8UL;
     numIters_ = parser.getOr("num_iters", 100000UL);
     reps_ = parser.getOr("reps", 3UL);
 
@@ -34,7 +34,7 @@ class SharedToRegisterBenchmark {
   std::string name() const { return benchmarkName; }
 
   void run() {
-    enc_["shared_to_register.csv"] << "bytes,threads,stride,iters,cycles,bandwidthGBps\n";
+    enc_["shared_to_register.csv"] << "run,bytes,threads,stride,iters,timeMs,bandwidthGBps\n";
 
     for (uint64_t bytes : sizes_) {
       uint32_t numElems = static_cast<uint32_t>(bytes / elemBytes_);
@@ -45,26 +45,21 @@ class SharedToRegisterBenchmark {
         if (threads > 1024)
           threads = 1024;
 
-        for (uint64_t stride : strides_) {
-          std::vector<uint64_t> cycles;
-          cycles.reserve(reps_);
-
-          for (uint64_t r = 0; r < reps_; ++r) {
-            uint64_t cycle = 0;
+        for (uint64_t r = 0; r < reps_; ++r) {
+          for (uint64_t stride : strides_) {
+            float ms = 0.0f;
             launchSharedToRegisterKernel(numElems, static_cast<uint32_t>(numIters_), static_cast<uint32_t>(threads),
-                                         bytes, static_cast<uint32_t>(stride), mode_, &cycle);
-            cycles.push_back(cycle);
+                                         bytes, static_cast<uint32_t>(stride), mode_, &ms);
+
+            const double transfersPerIter = (mode_ == sharedToRegister::READ_WRITE) ? 2.0 : 1.0;
+            const double bytesTransferred = static_cast<double>(elemBytes_) * transfersPerIter *
+                                            static_cast<double>(numIters_) * static_cast<double>(threads);
+            double bandwidthGBps = (bytesTransferred / (ms / 1000.0f)) / 1e9;
+  
+            enc_["shared_to_register.csv"] << r+1 << "," << bytes << "," << threads << "," << stride << "," << numIters_ << ","
+                                          << ms << "," << bandwidthGBps << "\n";
           }
 
-          const uint64_t avgCycles = std::accumulate(cycles.begin(), cycles.end(), 0) / (cycles.size());
-          const double seconds = (double)avgCycles / (double)clockFreq_;
-          const double transfersPerIter = (mode_ == sharedToRegister::READ_WRITE) ? 2.0 : 1.0;
-          const double bytesTransferred = static_cast<double>(elemBytes_) * transfersPerIter *
-                                          static_cast<double>(numIters_) * static_cast<double>(threads);
-          double bandwidthGBps = (bytesTransferred / seconds) / 1e9;
-
-          enc_["shared_to_register.csv"] << bytes << "," << threads << "," << stride << "," << numIters_ << ","
-                                         << avgCycles << "," << bandwidthGBps << "\n";
         }
       }
     }
