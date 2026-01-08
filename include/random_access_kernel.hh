c93317cf2aa16f8debf5c5e75e561cec282be2a3
Omar Hammoud <omar.hammoudx@gmail.com>
2026-01-08 14:52:47 +0200

    set new (same) default number of blocks for all levels

--- include/random_access_kernel.hh
+++ include/random_access_kernel.hh
@@ -29,12 +29,12 @@ static const constexpr uint64_t sensibleCacheSizes[MODES_SIZE] = {
 /**
  * sensibleNumBlocks - return a sensible default number of blocks for each mode
  *
- * The number of blocks is chosen as a multiple of 36, since we have 36 SMs.
+ * The default number of blocks is set to 36 for all memory levels, for a one-to-one mapping with SMs.
  */
 static const constexpr uint64_t sensibleNumBlocks[MODES_SIZE] = {
-    1,   /* L1 Cache. */
-    108, /* L2 Cache - 72 blocks underutilize the SMs while 144 blocks introduce overhead, making 108 the best balance. */
-    36, /* DRAM: provisional choice; other block counts will be analyzed to optimize it. */
+    36, /* L1 Cache. */
+    36, /* L2 Cache. */
+    36, /* DRAM. */
 };
 
 static __used mode parseMode(std::string& modeArg) {
